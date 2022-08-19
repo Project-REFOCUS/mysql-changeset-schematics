3f8399a8592cb822e22479164b7695c671cef49d
CThaw90 <christhaw90@gmail.com>
2022-08-18 20:59:10 -0400

    Populating the calendar_day table with days of the week

---
+++ calendar_day/populate.sql
@@ -0,0 +1,5 @@
+-- Populate the calendar_day table with day names
+INSERT INTO calendar_day (name, short_name) VALUES 
+	('Sunday', 'Sun'), ('Monday', 'Mon'), ('Tuesday', 'Tue'), ('Wednesday', 'Wed'),
+	('Thursday', 'Thu'), ('Friday', 'Fri'), ('Saturday', 'Sat')
+;
