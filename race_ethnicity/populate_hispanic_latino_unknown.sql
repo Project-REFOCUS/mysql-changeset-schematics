d21ccd0fedbca1dd256a9bef5e9cdafb3c1ecc22
CThaw90 <christhaw90@gmail.com>
2022-09-07 11:04:29 -0400

    Fix mismatch in column count

--- race_ethnicity/populate_hispanic_latino_unknown.sql
+++ race_ethnicity/populate_hispanic_latino_unknown.sql
@@ -1,2 +1,2 @@
 -- Populate race_ethnicity table with Hispanic or Latino and Unknown values
-INSERT INTO race_ethnicity (name) VALUES ('Hispanic or Latino', 'Unkown');
+INSERT INTO race_ethnicity (name) VALUES ('Hispanic or Latino'), ('Unkown');
