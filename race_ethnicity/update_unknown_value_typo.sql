4c3a3001bf1589fa21c1cac0ab4493123b13831c
CThaw90 <christhaw90@gmail.com>
2022-09-07 20:20:38 -0400

    Fixing typo in update_unknown_value_typo.sql script

--- race_ethnicity/update_unknown_value_typo.sql
+++ race_ethnicity/update_unknown_value_typo.sql
@@ -1,2 +1,2 @@
 -- Fix typo naming of Unkown value in the race_ethnicity table
-UPDATE race_ethnicity SET name = 'Unknown' WHERE name = 'Ukown';
+UPDATE race_ethnicity SET name = 'Unknown' WHERE name = 'Unkown';
