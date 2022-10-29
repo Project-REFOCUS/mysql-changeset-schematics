aac1802fcd481c4e21145ff9c2e0f16bbc6bd76a
cthaw <cthaw@mediaocean.com>
2022-10-29 09:40:03 -0400

    Updated script to include table name

--- covid_behind_bars/default_resident_active_staff_active_columns.sql
+++ covid_behind_bars/default_resident_active_staff_active_columns.sql
@@ -1,3 +1,3 @@
 -- Modify resident_active and staff_active column definitions
-ALTER TABLE MODIFY COLUMN resident_active SMALLINT UNSIGNED NULL DEFAULT '0';
-ALTER TABLE MODIFY COLUMN staff_active SMALLINT UNSIGNED NULL DEFAULT '0';
+ALTER TABLE covid_behind_bars MODIFY COLUMN resident_active SMALLINT UNSIGNED NULL DEFAULT '0';
+ALTER TABLE covid_behind_bars MODIFY COLUMN staff_active SMALLINT UNSIGNED NULL DEFAULT '0';
