dac48ad35d2c96c13b9545928748588b68c76a37
cthaw <cthaw@mediaocean.com>
2022-11-13 20:04:49 -0500

    Completed the schematic design for ingesting twitter data into the project refocus infrastructure

---
+++ twitter_account/add_foreign_key_to_twitter_account_type.sql
@@ -0,0 +1,2 @@
+-- Add link between twitter_account and it's corresponding account type
+ALTER TABLE ADD CONSTRAINT twitter_account_type_id_ibfk_1 FOREIGN KEY (twitter_account_type_id) REFERENCES twitter_account_type (id);
