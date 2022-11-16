d68dcb43ca8b2e7ef4ec579dc9c6c5fa7ea272f5
cthaw <cthaw@mediaocean.com>
2022-11-16 18:37:47 -0500

    Refactor twitter_terms_frequency table to point to tweets table instead of twitter_account

---
+++ twitter_terms_frequency/drop_column_calendar_date_id.sql
@@ -0,0 +1,2 @@
+-- Drop column calendar_date_id
+ALTER TABLE twitter_terms_frequency DROP COLUMN calendar_date_id;
---
+++ twitter_terms_frequency/drop_column_twitter_account_id.sql
@@ -0,0 +1,2 @@
+-- Drop foreign key twitter account
+ALTER TABLE twitter_terms_frequency DROP COLUMN twitter_account_id;
