fd778c9f9bb1f7cc03f8e7b849d1695ca1596018
CThaw90 <cthaw@mediaocean.com>
2023-04-24 22:14:04 -0400

    Add tables to track dataset from the environmental protection agency

---
+++ ej_data_field/create.sql
@@ -0,0 +1,6 @@
+-- Create ej_data_field table
+CREATE TABLE ej_data_field (
+	id TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL UNIQUE,
+	description TEXT NOT NULL UNIQUE
+);
---
+++ ej_data_value/create.sql
@@ -0,0 +1,9 @@
+-- Create ej_data_value table
+CREATE TABLE ej_data_value (
+	id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	year SMALLINT UNSIGNED NOT NULL,
+	value VARCHAR(20) NOT NULL,
+	ej_data_field_id TINYINT UNSIGNED NOT NULL,
+
+	FOREIGN KEY (ej_data_field_id) REFERENCES ej_data_field (id)
+);
