652a727ee0b97420d799d5d11338f337e8bfb4ec
CThaw90 <cthaw@mediaocean.com>
2023-05-02 22:06:45 -0400

    Updated ej_data_field primary key id to unsigned small int data type

--- ej_data_value/create.sql
+++ ej_data_value/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE ej_data_value (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	year SMALLINT UNSIGNED NOT NULL,
 	value VARCHAR(20) NOT NULL,
-	ej_data_field_id TINYINT UNSIGNED NOT NULL,
+	ej_data_field_id SMALLINT UNSIGNED NOT NULL,
 	block_group_id INTEGER UNSIGNED NOT NULL,
 
 	FOREIGN KEY (ej_data_field_id) REFERENCES ej_data_field (id),
