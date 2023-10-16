708dc206321b71bff76a880815794b4a6970ffa0
Josue Resilien <joshrez20@gmail.com>
2023-10-16 12:56:09 -0400

    Updated statement to fix bug

--- childcare_center/create.sql
+++ childcare_center/create.sql
@@ -5,8 +5,8 @@ CREATE TABLE childcare_center (
     zipcode_id SMALLINT UNSIGNED NOT NULL,
     status VARCHAR(30) NOT NULL,
     public_id VARCHAR(7) NOT NULL,
-    childcare_type_id SMALLINT UNSIGNED NOT NULL
+    childcare_type_id SMALLINT UNSIGNED NOT NULL,
 
     FOREIGN KEY (zipcode_id) REFERENCES zipcode (id),
-    FOREIGN KEY (childcare_type_id) REFERENCES childcare_type (id),
+    FOREIGN KEY (childcare_type_id) REFERENCES childcare_type (id)
 );
