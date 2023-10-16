93e19958ac1f02636459336ceff7e57d8e1b90f9
Josue Resilien <joshrez20@gmail.com>
2023-10-16 10:58:34 -0400

    Added wholesale market create statements

---
+++ wholesale_market_application_type/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE market_application_type (
+    id SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
---
+++ wholesale_markets/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE wholesale_market (
+    id SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    market VARCHAR(60) NOT NULL,
+    bic_number VARCHAR(15) NOT NULL,
+    account_name VARCHAR(100),
+    application_type_id SMALLINT UNSIGNED NOT NULL,
+    zipcode_id SMALLINT UNSIGNED NOT NULL,
+    disposition_date_id SMALLINT UNSIGNED NOT NULL,
+    effective_date_id SMALLINT UNSIGNED,
+    expiration_date_id SMALLINT UNSIGNED,
+
+    FOREIGN KEY (application_type_id) REFERENCES market_application_type (id),
+    FOREIGN KEY (zipcode_id) REFERENCES zipcode (id),
+    FOREIGN KEY (disposition_date_id) REFERENCES calendar_date (id),
+    FOREIGN KEY (effective_date_id) REFERENCES calendar_date (id),
+    FOREIGN KEY (expiration_date_id) REFERENCES calendar_date (id)
+);
