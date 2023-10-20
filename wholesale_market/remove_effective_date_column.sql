3ec8983179db3c4d331dc8c30ddc752f53a126fe
Josue Resilien <joshrez20@gmail.com>
2023-10-19 21:35:10 -0400

    Added wholesale foreign key removal statement

--- wholesale_market/remove_effective_date_column.sql
+++ wholesale_market/remove_effective_date_column.sql
@@ -1,2 +1,3 @@
 -- Removes the effective column for wholesale market
+ALTER TABLE wholesale_market DROP FOREIGN KEY wholesale_market_ibfk_4;
 ALTER TABLE wholesale_market DROP COLUMN effective_date_id;
