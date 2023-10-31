2afb0adf011206110055b5aa65f0339f21eda3bd
Josue Resilien <joshrez20@gmail.com>
2023-10-30 20:40:08 -0400

    Added wholesale account name unique constraint

--- wholesale_market/remove_date_columns.sql
+++ wholesale_market/remove_date_columns.sql
@@ -3,3 +3,4 @@ ALTER TABLE wholesale_market DROP FOREIGN KEY wholesale_market_ibfk_3;
 ALTER TABLE wholesale_market DROP COLUMN disposition_date_id;
 ALTER TABLE wholesale_market DROP FOREIGN KEY wholesale_market_ibfk_5;
 ALTER TABLE wholesale_market DROP COLUMN expiration_date_id;
+ALTER TABLE wholesale_market MODIFY COLUMN account_name VARCHAR(100) NOT NULL UNIQUE;
