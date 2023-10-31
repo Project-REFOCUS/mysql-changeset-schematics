9083ab30e32f3cbcd32327f8159a8423b3f27b52
Josue Resilien <joshrez20@gmail.com>
2023-10-30 21:19:28 -0400

    Added child center name unique constraint

--- childcare_center/remove_center_public_id_column.sql
+++ childcare_center/remove_center_public_id_column.sql
@@ -1,2 +1,3 @@
--- Removes the public id from the childcare center table. BIC_NUMBEr isnt a proper identifier
+-- Removes the public id from the childcare center table. BIC_NUMBER isnt a proper identifier
 ALTER TABLE childcare_center DROP COLUMN public_id;
+ALTER TABLE childcare_center MODIFY COLUMN center_name VARCHAR(180) NOT NULL UNIQUE;
