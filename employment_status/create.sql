6e690ad6d1d5b3d54491b3a877ac6bf192978685
CThaw90 <cthaw@mediaocean.com>
2023-11-02 18:21:14 -0400

    Changing the level of granularity for employment status to the census tract

--- employment_status/create.sql
+++ employment_status/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE employment_status (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	employed INTEGER UNSIGNED NOT NULL,
 	unemployed INTEGER UNSIGNED NOT NULL,
-	county_id SMALLINT UNSIGNED NOT NULL,
+	census_tract_id INTEGER UNSIGNED NOT NULL,
 
-	FOREIGN KEY (county_id) REFERENCES county (id)
+	FOREIGN KEY (census_tract_id) REFERENCES census_tract (id)
 );
