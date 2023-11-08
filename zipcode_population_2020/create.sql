fc27ab05688b0d7ac0d85ca7e3d6306f185b1c0a
Josue Resilien <joshrez20@gmail.com>
2023-11-07 22:23:52 -0500

    Added various population create statements

---
+++ zipcode_population_2020/create.sql
@@ -0,0 +1,8 @@
+-- Create zipcode_population_2020 table
+CREATE TABLE zipcode_population_2020 (
+	id SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	population INTEGER UNSIGNED NOT NULL,
+	zipcode_id SMALLINT UNSIGNED NOT NULL,
+
+	FOREIGN KEY (zipcode_id) REFERENCES zipcode (id)
+);
