738913e5d70a1e8701840e994c721f7572646ad6
Josue Resilien <joshrez20@gmail.com>
2023-11-08 22:40:22 -0500

    Added unique constraint to foreign key columns

--- block_group_population_2020/create.sql
+++ block_group_population_2020/create.sql
@@ -2,7 +2,7 @@
 CREATE TABLE block_group_population_2020 (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	population INTEGER UNSIGNED NOT NULL,
-	block_group_id INTEGER UNSIGNED NOT NULL,
+	block_group_id INTEGER UNSIGNED NOT NULL UNIQUE,
 
 	FOREIGN KEY (block_group_id) REFERENCES block_group (id)
 );
--- census_tract_population_2020/create.sql
+++ census_tract_population_2020/create.sql
@@ -2,7 +2,7 @@
 CREATE TABLE census_tract_population_2020 (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	population INTEGER UNSIGNED NOT NULL,
-	census_tract_id INTEGER UNSIGNED NOT NULL,
+	census_tract_id INTEGER UNSIGNED NOT NULL UNIQUE,
 
 	FOREIGN KEY (census_tract_id) REFERENCES census_tract (id)
 );
